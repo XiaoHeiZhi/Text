f6553ee2bfa5aff3ec3c595688be8e5a3e37a364
zxl <2729173112@qq.com>
2023-11-20 17:59:43 +0800

    test

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useTranslation } from "../i18n";
-import { Footer } from "./components/Footer";
 
 export default async function Page({
   params: { lng },
@@ -13,10 +12,9 @@ export default async function Page({
       <h1 className="w-36 text-center text-red-200 inline-block">
         {t("title")}
       </h1>
-      <Link href={`/${lng}/second-page`}>{t("to-second-page")}</Link>
-      <br />
-      <Link href={`/${lng}/client-page`}>{t("to-client-page")}</Link>
-      <Footer lng={lng} />
+      {/* <Link href={`/${lng}/second-page`}>{t("to-second-page")}</Link> */}
+      {/* <br /> */}
+      {/* <Link href={`/${lng}/client-page`}>{t("to-client-page")}</Link> */}
     </>
   );
 }
